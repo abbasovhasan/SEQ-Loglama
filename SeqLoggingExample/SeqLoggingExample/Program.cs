6051121607f0a7fe5e375da7dd4a2363dcdd5f45
Hasan Abbasov <abbasovhsn05@gmail.com>
2024-08-16 23:34:56 +0400

    first commit

---
+++ SeqLoggingExample/SeqLoggingExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // SEQ serverinə bağlanmaq üçün Serilog konfiqurasiyasını yaradın
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Seq("http://localhost:5341") // SEQ serverin URL adresi
+            .CreateLogger();
+
+        // Fərqli log səviyyələrində mesajlar yazın
+        Log.Information("Bu bir məlumat mesajıdır.");
+        Log.Warning("Bu bir xəbərdarlıq mesajıdır.");
+        Log.Error("Bu bir səhv mesajıdır.");
+
+        // Log məlumatlarını yumaq və sistemi təmizləmək üçün
+        Log.CloseAndFlush();
+    }
+}
